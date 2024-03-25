d41a10d93aae77525072f567f104fde9aac07b5f
Jacob King <jacobuid@gmail.com>
2024-03-25 13:14:38 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,81 @@
+import logo from './utils/abc-logo.svg';
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Uppercase from './routes/Uppercase';
+import Lowercase from './routes/Lowercase';
+
+function App() {
+    return (
+        <div className="app">
+            <header className="header background-dark-90 clearfix">
+                <div className="col-lg-4">
+                    <img
+                        src={logo}
+                        id="logo"
+                        className="width-60 inline-block margin-0 margin-y-5 margin-x-20"
+                        alt="logo"
+                    />
+                    <span
+                        id="name"
+                        className="inline-block font-30 padding-y-15"
+                    >
+                        read pixels
+                    </span>
+                </div>
+                <div className="col-lg-8 clearfix">
+                    <ul className="nav font-14 font-poppins clearfix margin-0 padding-0 pull-right-lg">
+                        <li>
+                            <a
+                                className="color-white color-white-hover padding-20"
+                                href="/uppercase"
+                            >
+                                Uppercase Letters
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                className="color-white color-white-hover padding-20"
+                                href="/lowercase"
+                            >
+                                Lowercase Letters
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                className="color-white color-white-hover padding-20"
+                                href="sight-words-1"
+                            >
+                                Sight Words 1
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                className="color-white color-white-hover padding-20"
+                                href="sight-words-2"
+                            >
+                                Sight Words 2
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                className="color-white color-white-hover padding-20"
+                                href="http://www.youtube.com"
+                            >
+                                Videos
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </header>
+            <main id="main">
+                <Routes>
+                    <Route path="/uppercase" element={<Uppercase />} />
+                    <Route path="/lowercase" element={<Lowercase />} />
+                </Routes>
+            </main>
+            <footer id="footer"></footer>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/utils/data.js
@@ -0,0 +1,64 @@
+function DEFAULT_UPPERCASE() {
+    return [
+        { value: 'A', flag: false },
+        { value: 'B', flag: false },
+        { value: 'C', flag: false },
+        { value: 'D', flag: false },
+        { value: 'E', flag: false },
+        { value: 'F', flag: false },
+        { value: 'G', flag: false },
+        { value: 'H', flag: false },
+        { value: 'I', flag: false },
+        { value: 'J', flag: false },
+        { value: 'K', flag: false },
+        { value: 'L', flag: false },
+        { value: 'M', flag: false },
+        { value: 'N', flag: false },
+        { value: 'O', flag: false },
+        { value: 'P', flag: false },
+        { value: 'Q', flag: false },
+        { value: 'R', flag: false },
+        { value: 'S', flag: false },
+        { value: 'T', flag: false },
+        { value: 'U', flag: false },
+        { value: 'V', flag: false },
+        { value: 'W', flag: false },
+        { value: 'X', flag: false },
+        { value: 'Y', flag: false },
+        { value: 'Z', flag: false },
+    ];
+}
+
+function DEFAULT_LOWERCASE() {
+    return [
+        { value: 'a', flag: false },
+        { value: 'b', flag: false },
+        { value: 'c', flag: false },
+        { value: 'd', flag: false },
+        { value: 'e', flag: false },
+        { value: 'f', flag: false },
+        { value: 'g', flag: false },
+        { value: 'h', flag: false },
+        { value: 'i', flag: false },
+        { value: 'j', flag: false },
+        { value: 'k', flag: false },
+        { value: 'l', flag: false },
+        { value: 'm', flag: false },
+        { value: 'n', flag: false },
+        { value: 'o', flag: false },
+        { value: 'p', flag: false },
+        { value: 'q', flag: false },
+        { value: 'r', flag: false },
+        { value: 's', flag: false },
+        { value: 't', flag: false },
+        { value: 'u', flag: false },
+        { value: 'v', flag: false },
+        { value: 'w', flag: false },
+        { value: 'x', flag: false },
+        { value: 'y', flag: false },
+        { value: 'z', flag: false },
+    ];
+}
+
+// 👇️ named exports
+export { DEFAULT_UPPERCASE, DEFAULT_LOWERCASE };
